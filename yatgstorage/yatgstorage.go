9e852d6e190b3280d9c156db7fe745484136e7fd
Vlad Lavrishko <lavrishkovlad@gmail.com>
2025-07-25 22:17:08 +0300

    feat(yatgstorage): set/get access hash/pts channel

--- yatgstorage/yatgstorage.go
+++ yatgstorage/yatgstorage.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/YaCodeDev/GoYaCodeDevUtils/yacache"
 	"github.com/YaCodeDev/GoYaCodeDevUtils/yaerrors"
@@ -18,6 +19,9 @@ const (
 	QtsPathRedisJSON  = BasePathRedisJSON + ".Qts"
 	DatePathRedisJSON = BasePathRedisJSON + ".Date"
 	SeqPathRedisJSON  = BasePathRedisJSON + ".Seq"
+
+	AccessHashFieldRedisHSet = "AccessHash"
+	PtsFieldRedisHSet        = "Pts"
 )
 
 type IStorage interface {
@@ -29,13 +33,11 @@ type IStorage interface {
 
 type Storage struct {
 	cache yacache.Cache[*redis.Client]
-	botID int
 }
 
-func NewStorage(cache yacache.Cache[*redis.Client], botID int) *Storage {
+func NewStorage(cache yacache.Cache[*redis.Client]) *Storage {
 	return &Storage{
 		cache: cache,
-		botID: botID,
 	}
 }
 
@@ -44,7 +46,7 @@ func (s *Storage) Ping(ctx context.Context) yaerrors.Error {
 }
 
 func (s *Storage) GetState(ctx context.Context, userID int64) (updates.State, bool, error) {
-	data, yaerr := s.cache.Raw().JSONGet(ctx, getKey(s.botID, userID)).Result()
+	data, yaerr := s.cache.Raw().JSONGet(ctx, getBotStorageKey(userID)).Result()
 	if yaerr != nil {
 		return updates.State{}, false, errors.Join(yaerr, ErrFailedToGetState)
 	}
@@ -53,14 +55,14 @@ func (s *Storage) GetState(ctx context.Context, userID int64) (updates.State, bo
 
 	err := json.Unmarshal([]byte(data), &state)
 	if err != nil {
-		errors.Join(err, ErrFailedToUnmarshalState)
+		return state, false, errors.Join(err, ErrFailedToUnmarshalState)
 	}
 
 	return state, true, nil
 }
 
 func (s *Storage) SetState(ctx context.Context, userID int64, state updates.State) error {
-	if err := s.cache.Raw().JSONSet(ctx, getKey(s.botID, userID), BasePathRedisJSON, state).Err(); err != nil {
+	if err := s.cache.Raw().JSONSet(ctx, getBotStorageKey(userID), BasePathRedisJSON, state).Err(); err != nil {
 		return errors.Join(err, ErrFailedToSetState)
 	}
 
@@ -68,7 +70,7 @@ func (s *Storage) SetState(ctx context.Context, userID int64, state updates.Stat
 }
 
 func (s *Storage) SetPts(ctx context.Context, userID int64, pts int) error {
-	if err := s.cache.Raw().JSONSet(ctx, getKey(s.botID, userID), PtsPathRedisJSON, pts).Err(); err != nil {
+	if err := s.cache.Raw().JSONSet(ctx, getBotStorageKey(userID), PtsPathRedisJSON, pts).Err(); err != nil {
 		return errors.Join(err, ErrFailedToSetPts)
 	}
 
@@ -76,7 +78,7 @@ func (s *Storage) SetPts(ctx context.Context, userID int64, pts int) error {
 }
 
 func (s *Storage) SetQts(ctx context.Context, userID int64, qts int) error {
-	if err := s.cache.Raw().JSONSet(ctx, getKey(s.botID, userID), QtsPathRedisJSON, qts).Err(); err != nil {
+	if err := s.cache.Raw().JSONSet(ctx, getBotStorageKey(userID), QtsPathRedisJSON, qts).Err(); err != nil {
 		return errors.Join(err, ErrFailedToSetQts)
 	}
 
@@ -84,7 +86,7 @@ func (s *Storage) SetQts(ctx context.Context, userID int64, qts int) error {
 }
 
 func (s *Storage) SetDate(ctx context.Context, userID int64, date int) error {
-	if err := s.cache.Raw().JSONSet(ctx, getKey(s.botID, userID), DatePathRedisJSON, date).Err(); err != nil {
+	if err := s.cache.Raw().JSONSet(ctx, getBotStorageKey(userID), DatePathRedisJSON, date).Err(); err != nil {
 		return errors.Join(err, ErrFailedToSetDate)
 	}
 
@@ -92,7 +94,7 @@ func (s *Storage) SetDate(ctx context.Context, userID int64, date int) error {
 }
 
 func (s *Storage) SetSeq(ctx context.Context, userID int64, seq int) error {
-	if err := s.cache.Raw().JSONSet(ctx, getKey(s.botID, userID), SeqPathRedisJSON, seq).Err(); err != nil {
+	if err := s.cache.Raw().JSONSet(ctx, getBotStorageKey(userID), SeqPathRedisJSON, seq).Err(); err != nil {
 		return errors.Join(err, ErrFailedToSetSeq)
 	}
 
@@ -100,7 +102,7 @@ func (s *Storage) SetSeq(ctx context.Context, userID int64, seq int) error {
 }
 
 func (s *Storage) SetDateSeq(ctx context.Context, userID int64, date, seq int) error {
-	key := getKey(s.botID, userID)
+	key := getBotStorageKey(userID)
 	if err := s.cache.Raw().
 		JSONMSet(ctx, key, DatePathRedisJSON, date, key, SeqPathRedisJSON, seq).Err(); err != nil {
 		return errors.Join(err, ErrFailedToSetDateSeq)
@@ -109,26 +111,65 @@ func (s *Storage) SetDateSeq(ctx context.Context, userID int64, date, seq int) e
 	return nil
 }
 
-func (s *Storage) GetChannelPts(ctx context.Context, userID, channelID int64) (pts int, found bool, err error) {
-	return 0, false, nil
-}
-
 func (s *Storage) SetChannelPts(ctx context.Context, userID, channelID int64, pts int) error {
+	if err := s.cache.Raw().
+		HSet(ctx, getChannelStorageKey(userID, channelID), PtsFieldRedisHSet, pts).Err(); err != nil {
+		return errors.Join(err, ErrFailedToSetChannelPts)
+	}
+
 	return nil
 }
 
-func (s *Storage) ForEachChannels(ctx context.Context, userID int64, f func(ctx context.Context, channelID int64, pts int) error) error {
+func (s *Storage) GetChannelPts(ctx context.Context, userID, channelID int64) (int, bool, error) {
+	data, yaerr := s.cache.HGet(ctx, getChannelStorageKey(userID, channelID), PtsFieldRedisHSet)
+	if yaerr != nil {
+		return 0, false, errors.Join(yaerr, ErrFailedToGetChannelPts)
+	}
+
+	res, err := strconv.ParseInt(data, 10, 0)
+	if err != nil {
+		return 0, false, errors.Join(yaerr, ErrFailedToParsePtsAsInt)
+	}
+
+	return int(res), true, nil
+}
+
+func (s *Storage) ForEachChannels(
+	_ context.Context,
+	_ int64,
+	_ func(ctx context.Context, channelID int64, pts int) error,
+) error {
 	return nil
 }
 
 func (s *Storage) SetChannelAccessHash(ctx context.Context, userID, channelID, accessHash int64) error {
+	if err := s.cache.Raw().
+		HSet(ctx, getChannelStorageKey(userID, channelID), AccessHashFieldRedisHSet, accessHash).Err(); err != nil {
+		return errors.Join(err, ErrFailedToSetChannelAccessHash)
+	}
+
 	return nil
 }
 
-func (s *Storage) GetChannelAccessHash(ctx context.Context, userID, channelID int64) (accessHash int64, found bool, err error) {
-	return 0, false, nil
+func (s *Storage) GetChannelAccessHash(ctx context.Context, userID, channelID int64) (int64, bool, error) {
+	data, err := s.cache.Raw().
+		HGet(ctx, getChannelStorageKey(userID, channelID), AccessHashFieldRedisHSet).Result()
+	if err != nil {
+		return 0, false, errors.Join(err, ErrFailedToGetChannelAccessHash)
+	}
+
+	res, err := strconv.ParseInt(data, 10, 64)
+	if err != nil {
+		return 0, false, errors.Join(err, ErrFailedToParseAccessHashAsInt64)
+	}
+
+	return res, true, nil
 }
 
-func getKey(botID int, userID int64) string {
-	return fmt.Sprintf("bot-storage:%d-%d", botID, userID)
+func getBotStorageKey(userID int64) string {
+	return fmt.Sprintf("bot-storage-state:%d", userID)
+}
+
+func getChannelStorageKey(userID int64, channelID int64) string {
+	return fmt.Sprintf("bot-channel-storage-state:%d-%d", userID, channelID)
 }
