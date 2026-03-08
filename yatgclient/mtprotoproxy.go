06c742056a5ac1a313ffe36373dc9e647f4999b4
YaCodesDevelopment <yacode@yacode.dev>
2026-03-08 16:46:40 +0200

    chore(lint): Update code for new CI version

--- yatgclient/mtprotoproxy.go
+++ yatgclient/mtprotoproxy.go
@@ -19,7 +19,7 @@ import (
 type MTProto struct {
 	Host   string
 	Port   uint16
-	Secret string //nolint:gosec // This struct is for parsing proxy config, so it has what it parses
+	Secret string
 }
 
 // NewMTProtoWithParseURL is a helper that allocates MTProto and calls ParseURL.
--- yatgclient/socks5proxy.go
+++ yatgclient/socks5proxy.go
@@ -19,7 +19,7 @@ type SOCKS5 struct {
 	Host     string
 	Port     uint16
 	Username *string
-	Password *string //nolint:gosec // This struct is for parsing proxy config, so it has what it parses
+	Password *string
 }
 
 // NewSOCKS5WithParseURL parses a socks5:// URL into a SOCKS5 struct(socks5://username:password@host:port)
